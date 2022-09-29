99ff4df2722c732fd5721bd4fe7fb137d335ad84
Developer Alamin <alaminduc516@gmail.com>
2022-09-29 20:35:28 +0600

    Complete Button added

--- src/components/Exercise-Details/ExerciseDetails.js
+++ src/components/Exercise-Details/ExerciseDetails.js
@@ -37,12 +37,20 @@ const ExerciseDetails = () => {
                     }
                 </div>
             </div>
-            <div className='exercise-details'>
+            <div>
                 <h3>Exercise Details</h3>
-                <div className="exercise-detail">
-                    
+                <div className='exercise-time'>
+                    <h4>Exercise time</h4>
+                    <p>200 seconds</p>
+                </div>
+                <div className='break-time'>
+                    <h4>Break time</h4>
+                    <p>30 seconds</p>
                 </div>
             </div>
+            <button className='complete-btn'>
+                Activity Completed
+            </button>
 
         </div>
     );
